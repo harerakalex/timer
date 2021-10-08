13316b5421de8bbb7e43ebff23c8798452554827
harerakalex <hareraloston@gmail.com>
2021-10-08 14:22:18 +0200

    Make minor improvement on logger

--- timer/utils/logger.py
+++ timer/utils/logger.py
@@ -2,38 +2,48 @@ import logging
 
 
 class CustomLogger:
+    default_level = logging.DEBUG
+
     def __init__(self, name: str):
         self.name = name
 
-    def log(self, level: str = "warning"):
-        # create logger
-        logger = logging.getLogger(self.name)
-        logger.setLevel(logging.DEBUG)
+    def __str__(self):
+        return "Logging in: {}".format(self.name)
 
-        # create console handler and set level to debug
+    def create_console_handler(self):
         ch = logging.StreamHandler()
-        ch.setLevel(logging.DEBUG)
+        ch.setLevel(self.default_level)
 
-        # create formatter
-        formatter = logging.Formatter(
-            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-        )
-
-        # add formatter to ch
+        formatter = self.create_formatter()
         ch.setFormatter(formatter)
 
-        # add ch to logger
+        return ch
+
+    def create_file_handler(self):
+        fh = logging.FileHandler(filename="{}.log".format(self.name))
+        fh.setLevel(self.default_level)
+
+        formatter = self.create_formatter()
+        fh.setFormatter(formatter)
+
+        return fh
+
+    def create_formatter(self):
+        return logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            datefmt="%m-%d-%Y %I:%M:%S %p",
+        )
+
+    def log(self, level: str = "warning", msg: str = "", file_log: bool = False):
+        logger = logging.getLogger(self.name)
+        logger.setLevel(self.default_level)
+
+        ch = self.create_console_handler()
+
         logger.addHandler(ch)
 
-        # 'application' code
-        if level == "debug":
-            print("======", type(logger))
-            logger["debug"]("debug message")
-        if level == "info":
-            logger.info("info message")
-        if level == "warning":
-            logger.warning("warn message")
-        if level == "error":
-            logger.error("error message")
-        if level == "critiacal":
-            logger.critical("critical message")
+        if file_log:
+            fh = self.create_file_handler()
+            logger.addHandler(fh)
+
+        return getattr(logger, level)(msg)
