b41e73d4c62047bb6e5f832ff3cde7e7fd5349a0
harerakalex <hareraloston@gmail.com>
2021-10-14 08:10:11 +0200

    Add timer decorator

--- timer/__init__.py
+++ timer/__init__.py
@@ -1,8 +1,40 @@
 __version__ = "0.1.0"
 
-from timer.utils.logger import CustomLogger
+import time
+from functools import wraps
+from typing import Any, Callable, Optional
 
+from timer.utils.logger import CustomLogger
 
 logger = CustomLogger(__name__)
 
-logger.log(level="debug")
+level = "info"
+
+
+def timer(
+    file_log: Optional[bool] = False, exp_time: Optional[float] = None
+) -> Callable[[Any], None]:
+    def decorator(func) -> Callable[[Any], None]:
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            finish_time = time.time()
+
+            used_time = finish_time - start_time
+
+            if exp_time is not None and exp_time <= used_time:
+                global level
+                level = "warning"
+
+            logger.log(
+                level=level,
+                msg="{} function ran in {:.2f} sec".format(func.__name__, used_time),
+                file_log=file_log,
+            )
+
+            return result
+
+        return wrapper
+
+    return decorator
